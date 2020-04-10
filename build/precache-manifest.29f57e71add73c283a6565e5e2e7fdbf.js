71827c755932046caa96b4c6aad33f7826c773d3
Thomas Pickert <ende.mail@web.de>
2020-04-10 17:37:04 -0400

    MOD: combined frontend/backend now works locally and remotely

---
+++ build/precache-manifest.29f57e71add73c283a6565e5e2e7fdbf.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a933fa314dc1b4cbc2ba8068fe016f96",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a9260436726e50e24e9b",
+    "url": "/static/css/main.08bda77d.chunk.css"
+  },
+  {
+    "revision": "cff202aa78e3a3bbc8d4",
+    "url": "/static/js/2.d214059d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.d214059d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a9260436726e50e24e9b",
+    "url": "/static/js/main.f598597f.chunk.js"
+  },
+  {
+    "revision": "cbe135e57de162cd240f",
+    "url": "/static/js/runtime-main.d0b0a789.js"
+  }
+]);
--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const cors = require('cors');
 
 app.use(cors());
 app.use(express.json());
+app.use(express.static('build'))
 
 morgan.token('body', (req, res) => (req.method == 'POST') ? JSON.stringify(req.body) : "");
 app.use(morgan(function (tokens, req, res) {
